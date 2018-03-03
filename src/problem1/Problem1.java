4b8e10713a148e009379e93f72680b35e1e27ed9
muntasir-mamun <mdmunnakhn6195@gmail.com>
2018-03-03 12:00:44 +0600

    init

---
+++ src/problem1/Problem1.java
@@ -0,0 +1,18 @@
+package problem1;
+
+public class Problem1 {
+   
+    public static void main(String[] args) {
+        float n=70;
+        float r=3;
+        
+        if (n>0 && r>0 && n>=r) {
+        System.out.println("Total Hour :" +n/r);
+        }
+        else {
+        System.out.println("Eror");
+        }
+        
+    }
+    
+}
